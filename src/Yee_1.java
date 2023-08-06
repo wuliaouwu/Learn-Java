d6b6218689b15132b1a0dee4a6f69f6fa899ce5c
wuliaouwu <wr339202808@outlook.com>
2023-08-07 00:45:49 +0800

    没啥用的数学小文件

---
+++ src/Yee_1.java
@@ -0,0 +1,10 @@
+public class Yee_1 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int J = a+b;
+        System.out.println(a+"+"+b+"="+J);
+    }
+
+}
