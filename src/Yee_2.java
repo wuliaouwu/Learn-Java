8f23f381b0f8685617d10599b2fc53dd36b785de
wuliaouwu <wr339202808@outlook.com>
2023-08-07 00:57:22 +0800

    没啥用的数学小文件,第二版

---
+++ src/Yee_2.java
@@ -0,0 +1,11 @@
+public class Yee_2 {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 1;
+        int J = a+b;
+        System.out.println("数学题目："+a+"+"+b);
+        System.out.println("算数结果："+a+"+"+b+"="+J);
+    }
+
+}
